0a2fb6f6fd05fd350d26d4dda139b382a3c5b562
alex lucero <a.lucero2892@gmail.com>
2013-11-08 15:08:31 -0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pressfrwrd::Application.config.secret_key_base = '51f069557d7e601af27a69c054bcd124d7aa445cef3bf8333dc9a37febe50617b7f989838d771bdbf01dcf85262d2581419916c426d27ef200dad3bef157d970'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pressfrwrd::Application.config.session_store :cookie_store, key: '_pressfrwrd_session'
